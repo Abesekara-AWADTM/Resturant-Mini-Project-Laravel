8b3120faf531b33e44b245ba982f57aede6871bf
‘Abesekara-AWADTM’ <thiliniabesekara1998@gmail.com>
2021-08-30 12:56:46 -0700

    login changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,8 @@ class HomeController extends Controller
     public function index(){
         return view("home");
     }
+
+    public function redirects(){
+    
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\HomeController;
 
 Route::get("/",[HomeController::class,"index"]);
 
+Route::get("/redirects",[HomeController::class,"redirects"]);
+
  Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
   return view('dashboard');
  })->name('dashboard');
