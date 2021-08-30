a68b85a1e16624be34eaad4cb1bdf6c986810249
‘Abesekara-AWADTM’ <thiliniabesekara1998@gmail.com>
2021-08-30 16:06:29 -0700

    admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -12,6 +13,13 @@ class HomeController extends Controller
     }
 
     public function redirects(){
-    
+        $usertype=Auth::user()->usertype;
+        if($usertype=='1'){
+            return view('admin.adminhome');
+        }
+        else
+        {
+            return view('home');
+        }
     }
 }
